da73645c071f3176e64410bc366dd581c401fca5
Ali <adadashipour@gmail.com>
2024-10-10 11:56:01 +0100

    Add Category and Trainer classes

---
+++ Vista.App/Data/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.App.Data
+{
+    public class Category
+    {
+        [Key] //Since the key name (CategoryCode) does not include ID,
+              //we have to use an annotation (could also specify this using FruitAPI)
+        [MaxLength(15)]
+        public required string CategoryCode { get; set; }
+        [MaxLength(30)] public string CategoryName { get; set; }
+        //Placeholder for List of Trainer Categories (many side of one-to-many)        
+    }
+}
---
+++ Vista.App/Data/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.App.Data
+{
+    public class Trainer
+    {
+        public int TrainerId { get; set; }
+        [MaxLength(50)]
+        public required string Name { get; set; }
+        [MaxLength(50)]
+        public string? Location { get; set; } //The question mark allow nulls
+        //Placeholder for List of Trainer Categories (many side one-to-many)
+        //Placeholder for List of Sessions (many side of one-to-many)
+    }
+}

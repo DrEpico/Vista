c4a594c637fe989f8c6c43c4eb6795e2e001d3f0
Ali <adadashipour@gmail.com>
2024-10-10 12:03:59 +0100

    Add Session and TrainerCategory

---
+++ Vista.App/Data/Session.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.App.Data
+{
+    public class Session
+    {
+        public int SessionId { get; set; }
+        public int TrainerId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime SessionDate { get; set; }
+
+        [MaxLength(40)]
+        public string? BookingReference { get; set; } //Allow nulls
+
+        //Placeholder for a navigation property to trainer (one side of one-to-many)
+    }
+}
---
+++ Vista.App/Data/TrainerCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vista.App.Data
+{
+    public class TrainerCategory
+    {
+        //Has a composite (compund) key that will be defined TrainersDbConext
+
+        [Required]
+        public int TrainerId { get; set; }
+        [MaxLength(15)] public required string CategoryCode { get; set; }
+
+        //Placeholder a navigation property to Trainer (one side of one-to-many)
+        // See TrainersDbConext for Foreign Key (Fluent API) definition
+        // Placeholder for a navigation property to Category (one side of one-to-many)
+    }
+}

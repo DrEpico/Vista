7181ca3dcdddc60c7f557cf2010670069b83158e
Ali <adadashipour@gmail.com>
2024-10-13 19:10:18 +0100

    List the Trainers

--- Vista.App/Program.cs
+++ Vista.App/Program.cs
@@ -3,6 +3,7 @@
 var trainerDbContext = new TrainersDbContext(); //Create databse context
 
 ListCategories(trainerDbContext);
+ListTrainers(trainerDbContext);
 
 //Method to List Categories from the database 
 static void ListCategories(TrainersDbContext dbContext)
@@ -18,4 +19,21 @@ static void ListCategories(TrainersDbContext dbContext)
         Console.WriteLine($"{category.CategoryCode} {category.CategoryName}");
     }
     Console.WriteLine();
+}
+
+// Method to List trainers from the database
+static void ListTrainers(TrainersDbContext dbContext)
+{
+    // Load list of categories from the database
+    var trainerList = dbContext.Trainers.ToList();
+
+    Console.WriteLine("Trainer List\n");
+
+    //Display list of categories from the memory object (trainerList)
+    foreach (var trainer in trainerList) 
+    {
+        Console.WriteLine($"{trainer.TrainerId} {trainer.Name}");
+    }
+    Console.WriteLine();
+
 }
